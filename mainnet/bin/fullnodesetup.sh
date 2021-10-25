fa60eb5fd8d02df292fae259a27cd5cf2cb49a22
manikanta <manikanta4a6@gmail.com>
2021-10-25 12:07:28 -0400

    changes to repo name

--- mainnet/bin/fullnodesetup.sh
+++ mainnet/bin/fullnodesetup.sh
@@ -1,13 +1,13 @@
       #!/bin/bash
       public_ip="$(curl ifconfig.me | awk '{$1=$1};1')" 
       internal_ip="$(hostname -I | awk '{$1=$1};1')"
-      config_data_url=https://raw.githubusercontent.com/Oneledger/fullnode/main/mainnet/v0.18
+      config_data_url=https://raw.githubusercontent.com/Oneledger/validator-deployment/main/mainnet/v0.18
       olclient=https://github.com/Oneledger/protocol/releases/download/v0.18.11/olclient
       olfullnode=https://github.com/Oneledger/protocol/releases/download/v0.18.11/olfullnode
       wget $olclient -O $GOROOT/bin/olclient && wget $olfullnode -O $GOROOT/bin/olfullnode && chmod +x $GOROOT/bin/*
       wget $config_data_url/genesis.json -O $OLDATA/genesis.json 
       $GOROOT/bin/olfullnode init --genesis $OLDATA/genesis.json --root $OLDATA
-      rm $OLDATA/consensus/config/genesis.json && wget https://raw.githubusercontent.com/Oneledger/fullnode/main/mainnet/v0.18/genesis.json -O $OLDATA/consensus/config/genesis.json
+      rm $OLDATA/consensus/config/genesis.json && wget https://raw.githubusercontent.com/Oneledger/validator-deployment/main/mainnet/v0.18/genesis.json -O $OLDATA/consensus/config/genesis.json
       wget $config_data_url/config.toml -O $OLDATA/config.toml
       sed -i "s/rpc_address.*/rpc_address = \"tcp:\/\/$internal_ip:26604\"/" $OLDATA/config.toml
       sed -i "s/p2p_address.*/p2p_address = \"tcp:\/\/$internal_ip:26605\"/" $OLDATA/config.toml
--- testnet/bin/fullnodesetup.sh
+++ testnet/bin/fullnodesetup.sh
@@ -1,7 +1,7 @@
       #!/bin/bash
       public_ip="$(curl ifconfig.me | awk '{$1=$1};1')" 
       internal_ip="$(hostname -I | awk '{$1=$1};1')"
-      config_data_url=https://raw.githubusercontent.com/Oneledger/fullnode/main/testnet/v0.18
+      config_data_url=https://raw.githubusercontent.com/Oneledger/validator-deployment/main/testnet/v0.18
       olclient=https://github.com/Oneledger/protocol/releases/download/v0.18.11/olclient
       olfullnode=https://github.com/Oneledger/protocol/releases/download/v0.18.11/olfullnode
       wget $olclient -O $GOROOT/bin/olclient && wget $olfullnode -O $GOROOT/bin/olfullnode && chmod +x $GOROOT/bin/*
